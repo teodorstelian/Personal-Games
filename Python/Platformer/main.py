0f8e3ad059b563cf8aade88e292cf35502e2669e
Teodor-Stelian Baltaretu <teodorstelian98@yahoo.com>
2022-11-08 01:08:17 +0200

    Added background + grid lines

--- Python/Platformer/main.py
+++ Python/Platformer/main.py
@@ -1,20 +1,42 @@
 import pygame
 from pygame import locals
 
+from settings import Settings
+
 pygame.init()
 
-screen_width = 1280
-screen_height = 720
+# Define settings
+screen_width = Settings.Width
+screen_height = Settings.Height
+tile_size = Settings.Tile_Size
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Test')
+
+# Load images
+background_img = pygame.image.load(Settings.Background)
+sun_img = pygame.image.load(Settings.Sun)
+
+
+def draw_grid():
+    for line in range(30):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
 
 run = True
 
 while run:
 
+    screen.blit(background_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    draw_grid()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
---
+++ Python/Platformer/settings.py
@@ -0,0 +1,11 @@
+class Settings:
+
+    # RESOLUTION
+    Width = 1280
+    Height = 720
+
+    Tile_Size = 50
+
+    # IMAGES
+    Background = 'resources/background.jpg'
+    Sun = 'resources/sun.png'
